d2db779399b0dd6365f9d5e142fe9233e64c02cd
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2023-03-14 13:52:41 +0100

    menu update
    >working menu with all of the views required
    >splashscreen changed
    >appicon changed

--- FileManager/MainPage.xaml.cs
+++ FileManager/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -11,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -22,9 +24,59 @@ namespace FileManager
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private NavigationViewItem _lastItem;
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            var item = args.InvokedItemContainer as NavigationViewItem;
+            if (item == null || item == _lastItem) return;
+
+            var clickedView = item.Tag?.ToString() ?? "SettingsView";
+            if (!NavigateToView(clickedView)) return;
+            _lastItem = item;
+            
+        }
+        
+        private bool NavigateToView(String clickedView)
+        {
+            var view = Assembly.GetExecutingAssembly().GetType($"FileManager.Views.{clickedView}");
+            if (view == null || String.IsNullOrWhiteSpace(clickedView)) return false;
+            
+            ContentFrame.Navigate(view, null, new EntranceNavigationTransitionInfo());
+            return true;
+        }
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (NavigationViewItemBase item in NavView.MenuItems)
+            {
+                if (item is NavigationViewItem && item.Tag.ToString() == "DefaultView")
+                {
+                    NavView.SelectedItem = item;
+                    break;
+                }
+            }
+            ContentFrame.Navigate(typeof(FileManager.Views.Default));
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            NavView.IsPaneOpen = false;
+        }
+
+        private void NavView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if(ContentFrame.CanGoBack)
+                ContentFrame.GoBack();
         }
     }
 }
