96b29d33549758cbe4ebde5924b691ad6237e8bb
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2023-03-24 20:48:01 +0100

    has permission to access files check

--- FileManager/Views/Default.xaml.cs
+++ FileManager/Views/Default.xaml.cs
@@ -25,6 +25,14 @@ namespace FileManager.Views
         public Default()
         {
             this.InitializeComponent();
+            if (App.NotHavePermission)
+            {
+                TextBoxDefaultView.Text = "If you see this message, the application does not have sufficient permissions to run properly. In order to solve this, the Settings App will be launched in ~20 seconds. Please search for the 'FileManager' app, select 'Advanced Options' from the 3 dots menu  on the right, and tick the 'File system' slider under the 'App permissions' tab. The app will close itself soon, please restart to use it."; 
+            }
+            else
+            {
+                TextBoxDefaultView.Text = "hello";
+            }
         }
     }
 }
