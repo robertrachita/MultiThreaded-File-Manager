862041add0a97382a28e34fe352c96d009e51d09
christostevan <72568727+christostevan@users.noreply.github.com>
2023-03-27 11:35:14 +0100

    Works

--- FileManager/Views/Comparator.xaml.cs
+++ FileManager/Views/Comparator.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
@@ -25,100 +24,124 @@ namespace FileManager.Views
 {
     public sealed partial class Comparator : Page
     {
+        private StorageFile file1;
         private byte[] byte1;
+        private StorageFile file2;
         private byte[] byte2;
         public Comparator()
         {
             this.InitializeComponent();
         }
 
-        private async void upload_file1(object sender, RoutedEventArgs e)
+        private async void filePick(int i)
         {
-            if (byte1 != null)
-            {
-                Array.Clear(byte1, 0, byte1.Length);
-            }
-
             var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
             picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
             picker.FileTypeFilter.Add("*");
-            using (Stream fileStr = await (await picker.PickSingleFileAsync()).OpenStreamForReadAsync())
+            StorageFile file = await picker.PickSingleFileAsync();
+            if (i == 1)
             {
-                byte[] bytes = new byte[fileStr.Length];
-                const int BUFFER_SIZE = 1024;
-                byte[] buffer = new byte[BUFFER_SIZE];
-                int position = 0;
-                int bytesread = 0;
-                while ((bytesread = await fileStr.ReadAsync(buffer, 0, BUFFER_SIZE)) > 0)
-                    for (int i = 0; i < bytesread; i++, position++)
-                        bytes[position] = buffer[i];
+                this.file1 = file;
+            }
+            else
+            {
+                this.file2 = file;
+            }
+        }
 
-                byte1 = new byte[bytes.Length];
-                Array.Copy(bytes, byte1, bytes.Length);
-                TextBoxCompare.Text = "";
-                foreach (var el in byte1)
+        private async void generateHash(StorageFile file)
+        {
+            if (file != null)
+            {
+                using (Stream fileStr = await file.OpenStreamForReadAsync())
                 {
-                    TextBoxCompare.Text += el.ToString();
+                    byte[] bytes = new byte[fileStr.Length];
+                    
+                    const int BUFFER_SIZE = 1024;
+                    byte[] buffer = new byte[BUFFER_SIZE];
+                    int position = 0;
+                    int bytesread = 0;
+                    while ((bytesread = await fileStr.ReadAsync(buffer, 0, BUFFER_SIZE)) > 0)
+                        for (int i = 0; i < bytesread; i++, position++)
+                            bytes[position] = buffer[i];
+                    if (file.Equals(file1))
+                    {
+                        byte1 = new byte[bytes.Length];
+                        Array.Copy(bytes, byte1, bytes.Length);
+                    }
+                    else
+                    {
+                        byte2 = new byte[bytes.Length];
+                        Array.Copy(bytes, byte2, bytes.Length);
+                    }
                 }
             }
         }
-        private async void upload_file2(object sender, RoutedEventArgs e)
+
+        private async void generate_hash2(StorageFile file)
         {
-            if (byte2 != null)
+            using (MemoryStream memStream = new MemoryStream())
             {
-                Array.Clear(byte2, 0, byte2.Length);
-            }
-
-            var picker = new Windows.Storage.Pickers.FileOpenPicker();
-            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
-            picker.FileTypeFilter.Add("*");
-            using (Stream fileStr = await (await picker.PickSingleFileAsync()).OpenStreamForReadAsync())
-            {
-                byte[] bytes = new byte[fileStr.Length];
-                const int BUFFER_SIZE = 1024;
-                byte[] buffer = new byte[BUFFER_SIZE];
-                int position = 0;
-                int bytesread = 0;
-                while ((bytesread = await fileStr.ReadAsync(buffer, 0, BUFFER_SIZE)) > 0)
-                    for (int i = 0; i < bytesread; i++, position++)
-                        bytes[position] = buffer[i];
-
-                byte2 = new byte[bytes.Length];
-                Array.Copy(bytes, byte2, bytes.Length);
-                TextBoxCompare.Text = "";
-                foreach (var el in byte2)
+                using (Stream fileStr = await file.OpenStreamForReadAsync())
                 {
-                    TextBoxCompare.Text += el.ToString();
+                    await fileStr.CopyToAsync(memStream);
+                    byte1 = memStream.ToArray();
                 }
             }
+        }
+
+        private void upload_file1(object sender, RoutedEventArgs e)
+        {
+            if (this.file1 != null)
+            {
+                this.file1 = null;
+            }
+            this.filePick(1);
+        }
+        private void upload_file2(object sender, RoutedEventArgs e)
+        {
+            if (this.file2 != null)
+            {
+                this.file2 = null;
+            }
+            this.filePick(2);
         }
 
         private void compare_files(object sender, RoutedEventArgs e)
         {
+            if (this.file1 == null || this.file2 == null)
+            {
+                return;
+            }
             TextBoxCompare.Text = "";
-            /*            Thread thread1 = new Thread(() => getByte(byte1, file1));
-                        Thread thread2 = new Thread(() => getByte(byte2, file2));
-                        thread1.Start();
-                        thread2.Start();
-                        thread1.Join();
-                        thread2.Join();*/
-            TextBoxCompare.Text = compare(byte1, byte2).ToString();
-            /*
-                        if (compare(byte1, byte2) == true)
-                        {
-                            TextBoxCompare.Text = "Files are the same";
-                        }
-                        else if (compare(byte1, byte2) == false) 
-                        {
-                            TextBoxCompare.Text = "Files are not the same";
-                        }*/
+
+            Thread t1 = new Thread(() => this.generateHash(this.file1));
+            Thread t2 = new Thread(() => this.generateHash(this.file2));
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            Thread.Sleep(1000);
+            if (compare(byte1, byte2))
+            {
+                TextBoxCompare.Text = "Files are the same";
+            }
+            else
+            {
+                TextBoxCompare.Text = "Files are not the same";
+            }
+            //this.generateHash(this.file1);
         }
 
         private bool compare(byte[] array1, byte[] array2)
         {
-            if (array1.Length != array2.Length) 
+            if (array1.Length != array2.Length)
                 return false;
-            
+
             for (int i = 0; i < array1.Length; i++)
             {
                 if (array1[i] != array2[i])
@@ -128,48 +151,6 @@ namespace FileManager.Views
             }
             return true;
         }
-
-        /*        private async Task getByte(byte[] array, StorageFile file)
-                {
-                    Array.Clear(byte1, 0, byte1.Length);
-                    Array.Clear(byte2, 0, byte2.Length);
-                    using (Stream fileStr = await (file.OpenStreamForReadAsync()))
-                    {
-                        byte[] bit = new byte[fileStr.Length];
-                        const int BUFFER_SIZE = 1024;
-                        byte[] buffer = new byte[BUFFER_SIZE];
-                        int position = 0;
-                        int bytesread = 0;
-                        while ((bytesread = await fileStr.ReadAsync(buffer, 0, BUFFER_SIZE)) > 0)
-                            for (int i = 0; i < bytesread; i++, position++)
-                                bit[position] = buffer[i];
-                        array = bit;
-                    }
-                }*/
-
-        private async void pickAFile()
-        {
-            TextBoxCompare.Text = "";
-            var picker = new Windows.Storage.Pickers.FileOpenPicker();
-            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
-            picker.FileTypeFilter.Add("*");
-            using (Stream fileStr = await (await picker.PickSingleFileAsync()).OpenStreamForReadAsync())
-            {
-                byte[] bytes = new byte[fileStr.Length];
-                const int BUFFER_SIZE = 1024;
-                byte[] buffer = new byte[BUFFER_SIZE];
-                int position = 0;
-                int bytesread = 0;
-                while ((bytesread = await fileStr.ReadAsync(buffer, 0, BUFFER_SIZE)) > 0)
-                    for (int i = 0; i < bytesread; i++, position++)
-                        bytes[position] = buffer[i];
-                foreach (var el in bytes)
-                {
-                    TextBoxCompare.Text += el.ToString();
-                }
-            }
-        }
-
         private string generateHash(string path)
         {
             var computedHash = HashAlgorithmNames.Md5;
@@ -189,5 +170,9 @@ namespace FileManager.Views
             return hex;
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            File.OpenRead(this.file1.Name);
+        }
     }
 }
